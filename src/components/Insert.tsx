d508f4bb9cf757c60ee5c31601f3d3f6f3abbcae
FBH514 <103460138+FBH514@users.noreply.github.com>
2023-02-21 18:05:19 -0500

    Adding insert methods

--- src/components/Insert.tsx
+++ src/components/Insert.tsx
@@ -1,9 +1,26 @@
+import {useEffect, useState} from "react";
+
 function Insert() {
 
     // left side: insert
     // right side: preview
 
     function InputFields() {
+        const [inputValues, setInputValues] = useState({
+            title: "",
+            location: "",
+            employer: "",
+            description: "",
+            url: "",
+        });
+        const [previewValues, setPreviewValues] = useState({
+            title: "input-title",
+            location: "input-location",
+            employer: "input-employer",
+            description: "input-description",
+            url: "input-url",
+        });
+        const [lock, setLock] = useState(true);
 
         const Input = [
             {name: "title", type: "text", placeholder: "Title"},
@@ -13,32 +30,76 @@ function Insert() {
             {name: "url", type: "text", placeholder: "URL"}
         ]
 
+        useEffect(() => {
+            if (inputValues.title !== "" && inputValues.location !== "" && inputValues.employer !== "" && inputValues.description !== "" && inputValues.url !== "") {
+                setLock(false);
+            } else {
+                setLock(true);
+            }
+        }, [inputValues]);
+
+        useEffect(() => {
+            document.getElementById("title-preview")!.innerHTML = inputValues.title;
+            document.getElementById("location-preview")!.innerHTML = inputValues.location;
+            document.getElementById("employer-preview")!.innerHTML = inputValues.employer;
+            document.getElementById("description-preview")!.innerHTML = inputValues.description;
+            document.getElementById("url-preview")!.innerHTML = inputValues.url;
+        }, [inputValues]);
+
+
+        function RemoveLock() {
+            setLock(false);
+        }
+
         return (
             <div id="insert-input-fields">
                 <div id="insert-input-fields-wrapper">
                     {Input.map((input, index) => {
                         return (
                             <div className="input-field" key={index}>
-                                <input type={input.type} name={input.name} placeholder={input.placeholder}/>
+                                <input
+                                    id={"input-" + input.name}
+                                    type={input.type}
+                                    name={input.name}
+                                    placeholder={input.placeholder}
+                                    onChange={(e) => {
+                                        setInputValues({...inputValues, [input.name]: e.target.value});
+                                    }}
+                                />
                             </div>
                         )
                     })}
                     <div className="button" id={"insert-submit-button"}>
-                        <button type="submit">Submit</button>
+                        <button type="submit" disabled={lock}></button>
                     </div>
                 </div>
             </div>
         );
     }
 
+    function Preview() {
+        return (
+            <div id="insert-preview">
+                <div id="insert-preview-wrapper">
+                    <h2 id={"title-preview"}>to do</h2>
+                    <h2 id={"location-preview"}>to do </h2>
+                    <h2 id={"employer-preview"}>to do</h2>
+                    <h2 id={"description-preview"}>to do</h2>
+                    <h2 id={"url-preview"}>to do</h2>
+                </div>
+            </div>
+        )
+    }
 
-    return(
+
+    return (
         <div id="insert-content">
             <div id={"insert-content-wrapper"}>
                 <div id={"insert-content-left"}>
                     <InputFields/>
                 </div>
                 <div id={"insert-content-right"}>
+                    <Preview/>
                 </div>
             </div>
         </div>
