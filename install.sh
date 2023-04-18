7a002317a522d34e07d930293107634d8757fa19
FBH514 <103460138+FBH514@users.noreply.github.com>
2023-04-18 17:09:52 -0400

    Added install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-pip install -r requirements.txt
+#!/bin/zsh
+
+cd server || exit
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+cd ../client || exit
+npm install
